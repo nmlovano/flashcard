abe4cf12fa934669e2d420252595b546c7caaea3
Sarah Hague <sarah.perdue07@gmail.com>
2019-08-06 13:47:02 -0400

    css stuff

--- vue_starter_java/frontend/src/App.vue
+++ vue_starter_java/frontend/src/App.vue
@@ -2,6 +2,9 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link>
+      <div>
+      <router-link to="/login">Login</router-link>
+      </div>
     </div>
     <router-view/>
   </div>
