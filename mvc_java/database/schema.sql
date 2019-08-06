4f0704f1669a1911d05e8972a0d1b33de24ea973
Christopher Horwitz <csmhorwitz@gmail.com>
2019-08-06 11:09:40 -0400

    SQL Work Chris H started

--- mvc_java/database/schema.sql
+++ mvc_java/database/schema.sql
@@ -5,14 +5,25 @@
 BEGIN;
 
 -- CREATE statements go here
-DROP TABLE IF EXISTS app_user;
+DROP TABLE IF EXISTS app_user CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
+
+DROP SEQUENCE IF EXISTS user_id;
 
 CREATE TABLE app_user (
-  id SERIAL PRIMARY KEY,
-  user_name varchar(32) NOT NULL UNIQUE,
-  password varchar(32) NOT NULL,
-  role varchar(32),
-  salt varchar(255) NOT NULL
+        id SERIAL PRIMARY KEY,
+        user_name varchar(32)  NOT NULL UNIQUE,
+        password  varchar(32)  NOT NULL,
+        role      varchar(32),
+        salt      varchar(255) NOT NULL
+);
+
+CREATE TABLE users (
+        user_id    integer          NOT NULL DEFAULT nextval('seq_user_id'),
+        password   varchar(15)      NOT NULL,
+        first_name varchar(25)      NOT NULL,
+        last_name  varchar(25)      NOT NULL,
+        CONSTRAINT pk_users_user_id PRIMARY KEY(user_id)
 );
 
 COMMIT;
