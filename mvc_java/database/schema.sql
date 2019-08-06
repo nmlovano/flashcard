5e24f5b6b89f6d051bbd7c40c76cd1b029dda461
Christopher Horwitz <csmhorwitz@gmail.com>
2019-08-06 13:59:19 -0400

    SQL almost finished

--- mvc_java/database/schema.sql
+++ mvc_java/database/schema.sql
@@ -5,25 +5,64 @@
 BEGIN;
 
 -- CREATE statements go here
-DROP TABLE IF EXISTS app_user CASCADE;
-DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS app_user     CASCADE;
+DROP TABLE IF EXISTS deck         CASCADE;
+DROP TABLE IF EXISTS card         CASCADE;
+DROP TABLE IF EXISTS deckscreated CASCADE;
+DROP TABLE IF EXISTS audit        CASCADE;
 
-DROP SEQUENCE IF EXISTS user_id;
+DROP SEQUENCE IF EXISTS seq_deck_id;        --deck table
+DROP SEQUENCE IF EXISTS seq_card_id;        --card table
+DROP SEQUENCE IF EXISTS seq_cool_secret_id; --deckscreated table
+DROP SEQUENCE IF EXISTS seq_session_id;     --audit table
+
+CREATE SEQUENCE seq_deck_id;
+CREATE SEQUENCE seq_card_id;
+CREATE SEQUENCE seq_cool_secret_id;
+CREATE SEQUENCE seq_session_id;
 
 CREATE TABLE app_user (
-        id SERIAL PRIMARY KEY,
-        user_name varchar(32)  NOT NULL UNIQUE,
-        password  varchar(32)  NOT NULL,
-        role      varchar(32),
-        salt      varchar(255) NOT NULL
+        email      text          NOT NULL,
+        user_name  varchar(32)   NOT NULL UNIQUE,
+        password   varchar(32)   NOT NULL,
+        is_admin   boolean       NOT NULL DEFAULT FALSE,
+        first_name varchar(25)   NOT NULL,
+        last_name  varchar(25)   NOT NULL,
+        salt       varchar(255)  NOT NULL
 );
 
-CREATE TABLE users (
-        user_id    integer          NOT NULL DEFAULT nextval('seq_user_id'),
-        password   varchar(15)      NOT NULL,
-        first_name varchar(25)      NOT NULL,
-        last_name  varchar(25)      NOT NULL,
-        CONSTRAINT pk_users_user_id PRIMARY KEY(user_id)
+CREATE TABLE deck (
+        deck_id                 integer              NOT NULL DEFAULT nextval('seq_deck_id'),   --primary key
+        deck_name               varchar(50)          NOT NULL,
+        deck_description        varchar(350)         NOT NULL
 );
+
+CREATE TABLE card (
+        card_id                 integer              NOT NULL DEFAULT nextval('seq_card_id'),   --primary key
+        deck_id                 integer              NOT NULL,                                  --primary key
+        card_front              varchar(100)         NOT NULL,
+        card_back               varchar(100)         NOT NULL,
+        card_tag                text                 NOT NULL
+);
+
+CREATE TABLE deckscreated(
+        cool_secret_id          integer              NOT NULL DEFAULT nextval('seq_cool_secret_id')
+        deck_id                 integer              NOT NULL,
+        email                   text                 NOT NULL
+);
+
+CREATE TABLE audit (
+        session_id              integer              NOT NULL DEFAULT nextval('seq_session_id'),--primary key
+        deck_id                 integer              NOT NULL,                                  --foreign key
+        num_right               integer              NOT NULL DEFAULT 0,
+        date                    date                 NOT NULL,
+        email                   text                 NOT NULL
+);
+
+ALTER TABLE audit               ADD FOREIGN KEY (deck_id) REFERENCES    deck(deck_id);
+ALTER TABLE audit               ADD FOREIGN KEY (email)   REFERENCES    users(email);
+ALTER TABLE card                ADD FOREIGN KEY (deck_id) REFERENCES    deck(deck_id);
+ALTER TABLE deckscreated        ADD FOREIGN KEY (deck_id) REFERENCES    deck(deck_id);
+ALTER TABLE deckscreated        ADD FOREIGN KEY (email)   REFERENCES    users(email);
 
 COMMIT;
