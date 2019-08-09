cb00c605b197b7843484968be28baddb325f617e
Nathan Lovano <nmlovano984@yahoo.com>
2019-08-09 11:14:20 -0400

    more controller work

--- vue_starter_java/backend/src/main/java/com/techelevator/controller/DeckAndCardController.java
+++ vue_starter_java/backend/src/main/java/com/techelevator/controller/DeckAndCardController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.techelevator.authentication.AuthProvider;
 import com.techelevator.dao.CardDeckDAO;
 import com.techelevator.model.CardDeck;
 
@@ -17,26 +18,42 @@ import com.techelevator.model.CardDeck;
 @CrossOrigin
 @RequestMapping("/deck")
 public class DeckAndCardController {
-	
+
+	private AuthProvider authProvider;
+
 	private CardDeckDAO cardDeckDAO;
-@Autowired
-public DeckAndCardController(CardDeckDAO cardDeckDAO) {
+
+	@Autowired
+	public DeckAndCardController(CardDeckDAO cardDeckDAO, AuthProvider authProvider) {
 		this.cardDeckDAO = cardDeckDAO;
+		this.authProvider = authProvider;
 	}
-@RequestMapping (path = "?username={username}", method = RequestMethod.GET)
-public List<CardDeck> getDeckByUsername(@RequestParam String username) {
-		return cardDeckDAO.getCardDecksByUserName(username); 
-}
-@RequestMapping (path = "", method = RequestMethod.POST)
-public void saveNewDeck(@RequestBody CardDeck deck, String name, String description) {
-	cardDeckDAO.saveCardDeck(name, description);
+
+	@RequestMapping(path = "?username={username}", method = RequestMethod.GET)
+	public List<CardDeck> getDeckByUsername(@RequestParam String username) {
+		return cardDeckDAO.getCardDecksByUserName(username);
+	}
+
+	@RequestMapping(path = "", method = RequestMethod.POST)
+	public void saveNewDeck(@RequestBody CardDeck deck, String name, String description) {
+		cardDeckDAO.saveCardDeck(name, description);
+
+	}
+
+	@RequestMapping(path = "?id={deckId}", method = RequestMethod.DELETE)
+	public void deleteDeck(@RequestParam int deckId) {
+		CardDeck thisDeck = cardDeckDAO.getCardDeckByDeckId(deckId);
+		if (thisDeck.getUserId() == authProvider.getCurrentUser().getId()) {
+			cardDeckDAO.deleteCardDeck(deckId);
+		}
+		System.out.println(authProvider.getCurrentUser().toString());
+
+	}
 	
-	
-}
-@RequestMapping (path = "", method = RequestMethod.DELETE)
-public void deleteDeck() {
-	
-	
-}
+	@RequestMapping(path = "?id={deckId}", method = RequestMethod.GET)
+	public CardDeck getDeckById(@RequestParam int deckId) {
+		CardDeck thisDeck = cardDeckDAO.getCardDeckByDeckId(deckId);
+		return thisDeck;
+	}
 
 }
