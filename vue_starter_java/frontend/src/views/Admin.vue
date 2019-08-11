965720c07cae7a008e7c4eb17f0a45c0583ba50b
Christopher Horwitz <csmhorwitz@gmail.com>
2019-08-11 12:19:44 -0400

    Linked Admin page to Decks page

--- vue_starter_java/frontend/src/views/Admin.vue
+++ vue_starter_java/frontend/src/views/Admin.vue
@@ -1,17 +1,17 @@
 
 <template>
-
 <div class="photo">
-  
   <div class="admin">
     <h1>Heyyyyyy</h1>
+    <form action="/Decks">
+        <button type="redirect">Add/Edit Decks Here!</button>
+    </form>
+  </div>
 </div>
-</div>
-
-
-
-
 </template>
+
+
+
 <style>
 
 .admin{
